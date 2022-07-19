40a2ca056b99a0df3ec1c168ead958734dc4f092
sarapapa-sp <shubhampawar1705@gmail.com>
2022-07-20 00:50:48 +0530

    Added the basis Spring batch files to add the data to inmemory database

---
+++ IPL-dashboard-backend/src/test/java/com/example/IPLdashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IPLdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
